fb55c859ca5db0565f63ec5ecd4f7c2e6684fe35
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2023-01-19 15:29:08 +0600

    connect mongo

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const express = require('express')
 const app = express()
-const port = 5000
 const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
 require('dotenv').config()
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4p5kw6q.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
-    // client.connect()
-    console.log("database connected");
+    try{
+      // const usersCollection = client.db("lens-lab").collection("users");
+    }
+    finally {
 
+    }
 }
-run()
+run().catch(console.log);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
