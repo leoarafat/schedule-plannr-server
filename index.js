e69c27ba4b869251fc2bc10dd408530a861bd823
Syeda-Aisha-Ahmad <18aishaahmad@gmail.com>
2023-01-20 20:49:03 +0600

    server

--- index.js
+++ index.js
@@ -13,19 +13,35 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4p5kw6q.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
-      const membershipCollection = client.db("ScheduPlannr").collection("membership");
+async function run() {
+  try {
+    const membershipCollection = client.db("ScheduPlannr").collection("membership");
+    const notesCollection = client.db("ScheduPlannr").collection("notes");
 
-      app.get('/membership', async(req, res)=>{
-        const query = {}
-        const result = await membershipCollection.find(query).toArray();
-        res.send(result)
-      })
-    }
-    finally {
+    app.get('/membership', async (req, res) => {
+      const query = {}
+      const result = await membershipCollection.find(query).toArray();
+      res.send(result)
+    })
 
-    }
+    // Add notes
+    app.post('/notes', async (req, res) => {
+      const query = req.body;
+      const result = await notesCollection.insertOne(query);
+      res.send(result);
+    })
+
+    // get notes
+    app.get('/notes', async (req, res) => {
+      const query = {};
+      const cursor = notesCollection.find(query);
+      const notes = await cursor.toArray();
+      res.send(notes)
+    })
+  }
+  finally {
+
+  }
 }
 run().catch(console.log);
 
