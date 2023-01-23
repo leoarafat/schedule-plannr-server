b172ff9d59ab3bfd6f1b56cdea7987e2a66834ee
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2023-01-23 19:51:33 +0600

    arafat

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-app.use(cors())
-app.use(express.json())
-require('dotenv').config()
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4p5kw6q.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-  const usersCollection = client.db('ScheduPlannr').collection('users')
+async function run() {
+  const usersCollection = client.db("ScheduPlannr").collection("users");
   app.post("/users", async (req, res) => {
     const user = req.body;
     const result = await usersCollection.insertOne(user);
     res.send(result);
   });
-
-    try{
-      // const usersCollection = client.db("lens-lab").collection("users");
-    }
-    finally {
-
-    }
+app.get('/users', async(req, res)=>{
+  const query = {};
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
+})
+  try {
+    // const usersCollection = client.db("lens-lab").collection("users");
+  } finally {
+  }
 }
 run().catch(console.log);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});
