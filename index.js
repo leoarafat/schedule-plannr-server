2f4215188815c29c8213324a70c9b4c63c06423c
moddas311 <moddasir311@gmail.com>
2023-01-27 16:19:44 +0600

    i dont't know what i did

--- index.js
+++ index.js
@@ -1,55 +1,70 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-app.use(cors())
-app.use(express.json())
-require('dotenv').config()
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4p5kw6q.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
   try {
     // membership collection
-    const membershipCollection = client.db("ScheduPlannr").collection("membership");
+    const membershipCollection = client
+      .db("ScheduPlannr")
+      .collection("membership");
     const notesCollection = client.db("ScheduPlannr").collection("notes");
 
     // User
     const userCollection = client.db("ScheduPlannr").collection("users");
 
     //time slots collection
-    const fifteenMinsAmCollection = client.db("ScheduPlannr").collection("fifteenMinsAM");
-    const fifteenMinsPmCollection = client.db("ScheduPlannr").collection("fifteenMinsPM");
-    const thirtyMinsAmCollection = client.db("ScheduPlannr").collection("thirtyMinsAM");
-    const thirtyMinsPmCollection = client.db("ScheduPlannr").collection("thirtyMinsPM");
-    const sixtyMinsAMCollection = client.db("ScheduPlannr").collection("sixtyMinsAM");
-    const sixtyMinsPmCollection = client.db("ScheduPlannr").collection("sixtyMinsPM");
-
+    const fifteenMinsAmCollection = client
+      .db("ScheduPlannr")
+      .collection("fifteenMinsAM");
+    const fifteenMinsPmCollection = client
+      .db("ScheduPlannr")
+      .collection("fifteenMinsPM");
+    const thirtyMinsAmCollection = client
+      .db("ScheduPlannr")
+      .collection("thirtyMinsAM");
+    const thirtyMinsPmCollection = client
+      .db("ScheduPlannr")
+      .collection("thirtyMinsPM");
+    const sixtyMinsAMCollection = client
+      .db("ScheduPlannr")
+      .collection("sixtyMinsAM");
+    const sixtyMinsPmCollection = client
+      .db("ScheduPlannr")
+      .collection("sixtyMinsPM");
 
     // Users
-    app.post('/users', async (req, res) => {
+    app.post("/users", async (req, res) => {
       const query = req.body;
       const result = await userCollection.insertOne(query);
       res.send(result);
-    })
+    });
 
-    app.get('/users', async (req, res) => {
-      const query = {}
+    app.get("/users", async (req, res) => {
+      const query = {};
       const result = await userCollection.find(query).toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     // Membership
-    app.get('/membership', async (req, res) => {
-      const query = {}
+    app.get("/membership", async (req, res) => {
+      const query = {};
       const result = await membershipCollection.find(query).toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     app.get("/membership/:id", async (req, res) => {
       const id = req.params.id;
@@ -62,72 +77,70 @@ async function run() {
     });
 
     // Add notes
-    app.post('/notes', async (req, res) => {
+    app.post("/notes", async (req, res) => {
       const query = req.body;
       const result = await notesCollection.insertOne(query);
       res.send(result);
-    })
+    });
 
     // get notes
-    app.get('/notes', async (req, res) => {
+    app.get("/notes", async (req, res) => {
       const query = {};
       const cursor = notesCollection.find(query);
       const notes = await cursor.toArray();
-      res.send(notes)
-    })
+      res.send(notes);
+    });
 
     // get 15mins time slots AM
-    app.get('/fifteenMinsAM', async (req, res) => {
+    app.get("/fifteenMinsAM", async (req, res) => {
       const query = {};
       const cursor = await fifteenMinsAmCollection.find(query).toArray();
       res.send(cursor);
-    })
+    });
 
     // get 15mins time slots PM
-    app.get('/fifteenMinsPM', async (req, res) => {
+    app.get("/fifteenMinsPM", async (req, res) => {
       const query = {};
       const cursor = await fifteenMinsPmCollection.find(query).toArray();
       res.send(cursor);
-    })
+    });
 
     // get 30mins time slots AM
-    app.get('/thirtyMinsAM', async (req, res) => {
+    app.get("/thirtyMinsAM", async (req, res) => {
       const query = {};
       const cursor = await thirtyMinsAmCollection.find(query).toArray();
       res.send(cursor);
-    })
+    });
 
     // get 30mins time slots PM
-    app.get('/thirtyMinsPM', async (req, res) => {
+    app.get("/thirtyMinsPM", async (req, res) => {
       const query = {};
       const cursor = await thirtyMinsPmCollection.find(query).toArray();
       res.send(cursor);
-    })
+    });
 
     // get 60mins time slots AM
-    app.get('/sixtyMinsAM', async (req, res) => {
+    app.get("/sixtyMinsAM", async (req, res) => {
       const query = {};
       const cursor = await sixtyMinsAMCollection.find(query).toArray();
       res.send(cursor);
-    })
+    });
 
     // get 60mins time slots PM
-    app.get('/sixtyMinsPM', async (req, res) => {
+    app.get("/sixtyMinsPM", async (req, res) => {
       const query = {};
       const cursor = await sixtyMinsPmCollection.find(query).toArray();
       res.send(cursor);
-    })
-  }
-  finally {
-
+    });
+  } finally {
   }
 }
 run().catch(console.log);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
