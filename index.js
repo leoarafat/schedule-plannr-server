854779734e87b3a224ac52300b083a63a0c22090
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2023-01-31 13:49:29 +0600

    complete user profile works

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ app.use(cors());
 app.use(express.json());
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const {
+  MongoClient,
+  ServerApiVersion,
+  ObjectId,
+  ObjectID,
+} = require("mongodb");
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4p5kw6q.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -74,6 +79,33 @@ async function run() {
       }
       const cursor = userCollection.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectID(id) };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+    app.put("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectID(id) };
+      const user = req.body;
+      const option = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: user.displayName,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          birthDate: user.birthDate,
+          contactNumber: user.contactNumber,
+          currentAddress: user.currentAddress,
+          permanentAddress: user.permanentAddress,
+          gender: user.gender,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, option);
       res.send(result);
     });
 
