99c7f69e25fbad896cc24601598beac3ca837472
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2023-02-06 18:14:10 +0600

    sdfg

--- index.js
+++ index.js
@@ -381,15 +381,21 @@ async function run() {
       }
     });
     // add blog
-    app.post("/blogs", verifyJWT, async (req, res) => {
+    app.post("/blogs", async (req, res) => {
       const query = req.body;
       const result = await blogsCollection.insertOne(query);
       res.send(result);
     });
-    app.get("/blogs", verifyJWT, async (req, res) => {
+    app.get("/blogs", async (req, res) => {
       const query = {};
       const result = await blogsCollection.find(query).toArray();
       res.send(result);
+    });
+    app.get("/blogPost/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const cursor = await blogsCollection.findOne(query);
+      res.send(cursor);
     });
   } finally {
   }
